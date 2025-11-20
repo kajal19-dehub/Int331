6c6c4ac8becda7b7a812e0d1e582f8ac3eb18b1e
kajal19-dehub <143339655+kajal19-dehub@users.noreply.github.com>
2025-11-20 13:47:10 +0530

    branch add

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
 hello world
+On branch master
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	.VirtualBox/
+	.android/
+	.bash_history
+	.cache/
+	.config/
+	.expo/
+	.gitconfig
+	.gradle/
+	.lesshst
+	.npmrc
+	.vscode/
+	AppData/
+	Contacts/
+	Documents/
+	Downloads/
+	Favorites/
+	IntelGraphicsProfiles/
+	Links/
+	Music/
+	NTUSER.DAT
+	NTUSER.DAT{2ad838bc-efea-11ee-a54d-000d3a94eaa1}.TM.blf
+	NTUSER.DAT{2ad838bc-efea-11ee-a54d-000d3a94eaa1}.TMContainer00000000000000000001.regtrans-ms
+	NTUSER.DAT{2ad838bc-efea-11ee-a54d-000d3a94eaa1}.TMContainer00000000000000000002.regtrans-ms
+	OneDrive/
+	Saved Games/
+	Searches/
+	Videos/
+	demo.tst
+	demo.txt
+	node_modules/
+	ntuser.dat.LOG1
+	ntuser.dat.LOG2
+	ntuser.ini
+	package-lock.json
+	package.json
+	server.js
+	shopping-chatbot/
+
+nothing added to commit but untracked files present (use "git add" to track)
