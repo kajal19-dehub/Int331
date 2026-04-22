c5be6d5aa373ea056cc1d43cacfe22abea417d9f
kajal19-dehub <143339655+kajal19-dehub@users.noreply.github.com>
2026-04-22 20:43:00 +0530

    fix google callback production

--- Downloads/bizconnect-directory-main/bizconnect-directory-main/backend/config/passport.js
+++ Downloads/bizconnect-directory-main/bizconnect-directory-main/backend/config/passport.js
@@ -12,7 +12,10 @@ console.log('=======================================\n');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL // Hardcode for now
+   callbackURL:
+   process.env.NODE_ENV === "production"
+    ? process.env.GOOGLE_CALLBACK_URL
+    : "http://localhost:5000/api/auth/google/callback" // Hardcode for now
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
