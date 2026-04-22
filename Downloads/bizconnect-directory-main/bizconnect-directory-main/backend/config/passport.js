cea0c21a45c4c188f89e9323d52d976a5c2f4cda
kajal19-dehub <143339655+kajal19-dehub@users.noreply.github.com>
2026-04-22 21:02:02 +0530

    final fix google oauth

--- Downloads/bizconnect-directory-main/bizconnect-directory-main/backend/config/passport.js
+++ Downloads/bizconnect-directory-main/bizconnect-directory-main/backend/config/passport.js
@@ -9,6 +9,7 @@ console.log('Google Client Secret exists:', !!process.env.GOOGLE_CLIENT_SECRET);
 console.log('=======================================\n');
 
 // Google Strategy
+console.log("ENV CALLBACK:", process.env.GOOGLE_CALLBACK_URL);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
