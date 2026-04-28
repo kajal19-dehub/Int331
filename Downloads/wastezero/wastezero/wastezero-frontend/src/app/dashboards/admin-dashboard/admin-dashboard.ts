72b8292591a9237f17dd67fc4551cf8ad244d13f
kajal19-dehub <143339655+kajal19-dehub@users.noreply.github.com>
2026-04-28 23:38:48 +0530

    frontend changes

---
+++ Downloads/wastezero/wastezero/wastezero-frontend/src/app/dashboards/admin-dashboard/admin-dashboard.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.css'
+})
+export class AdminDashboard {
+
+  totalOpportunities = 12;
+  totalVolunteers = 34;
+  totalApplications = 21;
+
+}
---
+++ Downloads/wastezero/wastezero/wastezero-frontend/src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'https://wastezero-u0si.onrender.com/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: any) {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+
+  register(data: any) {
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+}
